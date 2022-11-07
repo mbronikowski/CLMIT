4c834edb26614a48735de7be0658e8fd7bd1315b
Mateusz Bronikowski <37741410+mbronikowski@users.noreply.github.com>
2022-11-07 18:39:25 +0100

    Added arcsec deflection maps support
    Added arcsec- and pixel- deflection map support and conversion between them, greatly optimized caustic area and critical area map generation. Further development.

--- clmi.py
+++ clmi.py
@@ -1,14 +1,14 @@
-import numpy as np
-import queue
 import cv2
+import numpy as np
+# import queue
 import warnings
 
-from astropy.wcs import WCS
+from astropy import constants, units, wcs
 from astropy.cosmology import FlatLambdaCDM
 from astropy.io import fits
+from scipy.ndimage.measurements import label
 
-_cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
-_C = 299792458  # speed of light, SI units      # TODO: astropy units handling etc?
+_cosmology = FlatLambdaCDM(H0=70, Om0=0.3)
 _dilation_erosion_steps = 1                     # TODO: fine tune this
 
 
@@ -19,32 +19,32 @@ def set_cosmology(new_cosmology):
     needs to be set in the instance to reset cosmology-dependent calculations.
     """
 
-    global _cosmo
-    _cosmo = new_cosmology
+    global _cosmology
+    _cosmology = new_cosmology
 
 
-def distance_scale_factor(z_lens, z_source):
+def lensing_scaling_factor(z_lens, z_source):
     """Ratio of angular diameter distances from lens to source and from observer to source.
 
     The ratio is used to scale deflection and magnification maps. 
     Uses module level defined cosmology, which can be redefined using set_cosmology.
     """
 
-    dist_deflector_source = _cosmo.angular_diameter_distance_z1z2(z_lens, z_source)     # commonly referred to as Dds.
-    dist_source = _cosmo.angular_diameter_distance_z1z2(0, z_source)                    # Commonly referred to as Ds.
+    dist_deflector_source = _cosmology.angular_diameter_distance_z1z2(z_lens, z_source)   # commonly referred to as Dds.
+    dist_source = _cosmology.angular_diameter_distance_z1z2(0, z_source)                  # Commonly referred to as Ds.
     return dist_deflector_source / dist_source
 
 
-def distance_parameter(z_lens, z_source):
+def distance_parameter(z_lens, z_source):       # TODO: rename?
     """Used in time delay equation, see https://arxiv.org/pdf/astro-ph/9606001.pdf Eq. 63.
 
     Uses module level defined cosmology, which can be redefined using set_cosmology.
     """
 
-    dist_deflector_source = _cosmo.angular_diameter_distance_z1z2(z_lens, z_source)
-    dist_source = _cosmo.angular_diameter_distance_z1z2(0, z_source)
-    dist_deflector = _cosmo.angular_diameter_distance_z1z2(0, z_lens)
-    return (dist_deflector * dist_source / dist_deflector_source).value
+    dist_deflector_source = _cosmology.angular_diameter_distance_z1z2(z_lens, z_source)
+    dist_source = _cosmology.angular_diameter_distance_z1z2(0, z_source)
+    dist_deflector = _cosmology.angular_diameter_distance_z1z2(0, z_lens)
+    return dist_deflector * dist_source / dist_deflector_source
 
 
 def magnification(kappa, gamma, scaling_factor):  # Works for both individual numbers and np arrays.
@@ -61,58 +61,28 @@ def time_delay(z_lens, dist_param, alpha, psi):             # TODO: fix this, wh
     psi: Gravitational lensing potential
     Citation: https://arxiv.org/pdf/astro-ph/9606001.pdf equation 63
     """
-    return (1 + z_lens) / _C * dist_param * (.5 * (alpha ** 2) - psi)
+
+    return (1 + z_lens) / constants.c * dist_param * (.5 * (alpha ** 2) - psi)
 
 
-_direction_set_4 = np.array([(-1, 0), (1, 0), (0, -1), (0, 1)])  # Used in flood_fill to disable diagonals
-_direction_set_8 = np.array([(-1, 0), (1, 0), (0, -1), (0, 1),   # Used in flood_fill to enable diagonals
-                             (-1, -1), (-1, 1), (1, -1), (1, 1)])
-
-
-def _flood_fill(array, start_pixel, targeted_val, new_val, directions=4):
+def _flood_fill(array, start_pixel, targeted_val, new_val):
     """Flood fill tool on a numpy array using a queue."""
-
-    def handle_pixel(pixel_coords):
-        if (new_arr[pixel_coords]) == targeted_val:
-            new_arr[pixel_coords] = new_val
-            for i in range(directions):
-                new_coords = pixel_coords + dir_set[i]
-                if ((0, 0) <= new_coords).all() and (
-                        new_coords < new_arr.shape).all():  # if new pixel coords are in array:
-                    new_coords = tuple(new_coords)
-                    if new_arr[new_coords] == targeted_val:
-                        pixel_queue.put(new_coords)
-
-    if array[start_pixel] != targeted_val:  # Maybe just remove this and make it take the starting val?
-        raise ValueError("Start pixel has value "  # Or assume targeted_val = array[start] by default?
-                         + str(array[start_pixel])
-                         + ", but targeted value is "
-                         + str(new_val)
-                         + "!")
-
-    if directions == 4:  # only side-adjacent pixels
-        dir_set = _direction_set_4
-    elif directions == 8:  # include diagonal pixels
-        dir_set = _direction_set_8
-    else:
-        raise ValueError("'directions' needs to be 4 or 8.")
-        # Can be replaced with 'dir_set = directions' to allow custom direction sets
-
+    if array[start_pixel] != targeted_val:          # Sanity check
+        raise ValueError("The flood fill tool has failed. Array had value of " +
+                         str(array[start_pixel]) + ", expected " + str(targeted_val) + ".")
     new_arr = np.copy(array)
-    pixel_queue = queue.SimpleQueue()
-    pixel_queue.put(start_pixel)
-    while not pixel_queue.empty():
-        handle_pixel(pixel_queue.get())
-
+    label_map = label(new_arr == new_arr[start_pixel])[0]
+    new_arr[label_map == label_map[start_pixel]] = new_val
     return new_arr
 
 
+# noinspection SpellCheckingInspection
 class ClusterModel:
     """Handle individual cluster models and calculations based on them."""
-    def __init__(self, cluster_z, working_z=9, kappa_file=None, gamma_file=None,
+    def __init__(self, cluster_z, source_z=9, kappa_file=None, gamma_file=None,
                  psi_file=None, x_pixel_deflect_file=None, y_pixel_deflect_file=None,
-                 x_as_deflect_file=None, y_as_deflect_file=None, wcs=None):
-        self.wcs = wcs
+                 x_as_deflect_file=None, y_as_deflect_file=None, map_wcs=None):
+        self.wcs = map_wcs
         self.kappa_map = self._check_and_load_data(kappa_file)
         self.gamma_map = self._check_and_load_data(gamma_file)
         self.psi_map = self._check_and_load_data(psi_file)
@@ -122,81 +92,108 @@ class ClusterModel:
         self.y_as_deflect_map = self._check_and_load_data(y_as_deflect_file)
 
         self.cluster_z = cluster_z
-        self.working_z = working_z
-        self.distance_ratio = distance_scale_factor(self.cluster_z, self.working_z)
-        self.distance_param = distance_parameter(self.cluster_z, self.working_z)
-        self.magnification_map = None
-        self.critical_area_map = None
-        self.caustic_area_map = None        # This map has the same angular scale as lens plane maps.
+        self.source_z = source_z
+        self.cluster_angular_diameter_distance = _cosmology.angular_diameter_distance_z1z2(0, self.cluster_z)
+        self.source_angular_diameter_distance = _cosmology.angular_diameter_distance_z1z2(0, self.source_z)
+        self.distance_ratio = lensing_scaling_factor(self.cluster_z, self.source_z)
+        self.distance_param = distance_parameter(self.cluster_z, self.source_z)
+        self._magnification_map = None       # Private. Access through get_[type]_map.
+        self._critical_area_map = None
+        self._caustic_area_map = None        # This map has the same angular scale as lens plane maps.
         # TODO: add checking deflection map units (arcsec, pixel etc) and conversion handling (can you use astropy?)
         if self.wcs is None:
             warnings.warn("The cluster model was unable to load a working World Coordinate System from provided files.")
 
     def _check_and_load_data(self, initial_input):
         def add_wcs(fits_header):
-            # Load WCS from FITS if the object doesn't have one yet:
-            if self.wcs is None and "CRVAL1" in fits_header.keys():  # This is a bad (?) way to check
-                self.wcs = WCS(fits_header)                          # if a working WCS can be generated.
+            # Load WCS from FITS and set object WCS if the loaded one is valid:
+            new_wcs = wcs.WCS(fits_header)
+            if new_wcs.has_celestial:
+                self.wcs = new_wcs
 
         if initial_input is None:       # If no file provided, keep None.
             return None
         elif isinstance(initial_input, str):          # If filename str is provided, open file, load data, close file.
             with fits.open(initial_input) as fits_file:
                 data_table = fits_file[0].data.copy()
-                add_wcs(fits_file[0].header)
+                if self.wcs is None:
+                    add_wcs(fits_file[0].header)
             return data_table
         # Otherwise, assume a fits file was provided. Copy data from it in case it's closed while the object is in use.
         add_wcs(initial_input[0].header)
         return initial_input[0].data.copy()   # TODO: [0], ['SCI'] or something else? How to guarantee loading?
-        # TODO: passing np tables with data?
+        # TODO: functionality for passing np tables with data?
 
-    def set_working_z(self, new_z):
-        self.working_z = new_z
-        self.distance_ratio = distance_scale_factor(self.cluster_z, new_z)
+    def set_source_z(self, new_z):
+        """Set the redshift for which parameters will be calculated and reset previously computed z-dependent values."""
+        self.source_z = new_z
+        self.source_angular_diameter_distance = _cosmology.angular_diameter_distance_z1z2(0, self.source_z)
+        self.distance_ratio = lensing_scaling_factor(self.cluster_z, new_z)
         self.distance_param = distance_parameter(self.cluster_z, new_z)
-        self.magnification_map = None       # Reset all z-specific data. It will be generated as needed.
-        self.critical_area_map = None
-        self.caustic_area_map = None
+        self._magnification_map = None       # Reset all z-specific data. It will be generated as needed.
+        self._critical_area_map = None
+        self._caustic_area_map = None
+
+    def _generate_arcsec_deflect_maps(self):
+        pixel_scale = self.wcs.proj_plane_pixel_scales()[0].to(units.arcsec).value
+        self.x_as_deflect_map = self.x_pixel_deflect_map.copy() * pixel_scale
+        self.y_as_deflect_map = self.y_pixel_deflect_map.copy() * pixel_scale
+
+    def _generate_pixel_deflect_maps(self):
+        pixel_scale = self.wcs.proj_plane_pixel_scales()[0].to(units.arcsec).value
+        self.x_pixel_deflect_map = self.x_as_deflect_map / pixel_scale
+        self.y_pixel_deflect_map = self.y_as_deflect_map / pixel_scale
 
     def get_magnification_map(self):
-        if self.magnification_map is None:
+        if self._magnification_map is None:
             self._generate_magnification_map()
-        return self.magnification_map
+        return self._magnification_map
 
     def _generate_magnification_map(self):
-        self.magnification_map = magnification(self.kappa_map, self.gamma_map, self.distance_ratio)
+        self._magnification_map = magnification(self.kappa_map, self.gamma_map, self.distance_ratio)
 
     def get_critical_area_map(self):
-        if self.critical_area_map is None:
+        if self._critical_area_map is None:
             self._generate_critical_area_map()
-        return self.critical_area_map
+        return self._critical_area_map
 
     def _generate_critical_area_map(self):
         magnif_map = self.get_magnification_map()
         magnif_sign = np.ones(magnif_map.shape, dtype=int)      # Get map of magnification signs, get rid of outer area.
         magnif_sign[np.where(magnif_map < 0.)] = -1
-        magnif_sign = _flood_fill(magnif_sign, (0, 0), 1, 0)       # Note: we're assuming (0, 0) is outside
-        self.critical_area_map = np.abs(magnif_sign)                # the critical curve. If it isn't, this will crash.
+        magnif_sign = _flood_fill(magnif_sign, (0, 0), 1, 0)         # Note: we're assuming (0, 0) is outside
+        self._critical_area_map = np.abs(magnif_sign)                # the critical curve. If it isn't, this will crash.
 
     def get_caustic_area_map(self):
-        if self.caustic_area_map is None:
-            self._generate_caustic_area_map()
-        return self.caustic_area_map
+        if self._caustic_area_map is None:
+            self._caustic_area_map = self.map_to_source_plane(self.get_critical_area_map())
+        return self._caustic_area_map
 
-    def _generate_caustic_area_map(self):       # Here we start getting prone to numerical issues.
-        critical_map = self.get_critical_area_map()     # TODO: double check that this works on all data.
-        hit_map = np.zeros(critical_map.shape)
+    def map_to_source_plane(self, lens_plane_map):
+        """Map arbitrary area from the lens plane to the source plane.
+
+        The input needs to be a numpy array of integers with 1s for pixels which are to be mapped, 0s otherwise.
+        The function will fill in any holes in the mapped source plane area; the function does not handle areas with
+        holes in them.
+        """
+
+        if self.x_as_deflect_map is None or self.y_as_deflect_map is None:
+            self._generate_pixel_deflect_maps()
+        if lens_plane_map.shape != self.x_pixel_deflect_map.shape:
+            raise ValueError("The array to be mapped back to the source plane needs to have the same shape \
+                as deflection maps.")
+        hit_map = np.zeros(lens_plane_map.shape)
         mapping_warning_issued = False
-        for y in range(critical_map.shape[0]):
-            for x in range(critical_map.shape[1]):
-                if critical_map[y, x] > 0:
-                    y_hit = int(y + self.y_pixel_deflect_map[y, x] * self.distance_ratio)     # This assumes deflect maps
-                    x_hit = int(x + self.x_pixel_deflect_map[y, x] * self.distance_ratio)     # are given in pixels.
+        for y in range(lens_plane_map.shape[0]):
+            for x in range(lens_plane_map.shape[1]):
+                if lens_plane_map[y, x] > 0:
+                    y_hit = int(y + self.y_pixel_deflect_map[y, x] * self.distance_ratio)   # This assumes deflect maps
+                    x_hit = int(x + self.x_pixel_deflect_map[y, x] * self.distance_ratio)   # are given in pixels.
                     if 0 <= y_hit < hit_map.shape[0] and 0 <= x_hit < hit_map.shape[1]:
                         hit_map[y_hit, x_hit] = 1
                     elif not mapping_warning_issued:
-                        warnings.warn("Some of the area inside the critical curve maps \
-                                      outside the generated source plane map.")
+                        warnings.warn("Some of the area passed to this function is mapped \
+                                      outside the generated source plane map. This WILL affect area calculations.")
                         mapping_warning_issued = True
         # Now we have a raw map from the critical to caustic curve. We need to make sure flood fill doesn't fill gaps
         # between individual hits.
@@ -204,5 +201,17 @@ class ClusterModel:
         hit_map = cv2.erode(hit_map, None, iterations=_dilation_erosion_steps)
         # Now hopefully all of the gaps between hits were filled and we can flood fill:
         hit_map = _flood_fill(hit_map, (0, 0), 0, -1)       # -1 is the outer area outside the caustic curve.
-        self.caustic_area_map = np.zeros(hit_map.shape)     # 0 and 1 are inside.
-        self.caustic_area_map[np.where(hit_map >= 0)] = 1   # The final map has 1 inside, 0 outside the caustic.
+        source_plane_map = np.zeros(hit_map.shape)     # 0 and 1 are inside.
+        source_plane_map[np.where(hit_map >= 0)] = 1   # The final map has 1 inside, 0 outside the caustic.
+        return source_plane_map.astype(int)
+
+    def corresponding_plane_area(self, pixel_map, plane_redshift):
+        """Calculate the area marked in pixel_map at given redshift.
+
+        pixel_map needs to contain integer 1s where counting is required."""
+        dist_source = _cosmology.angular_diameter_distance_z1z2(0, plane_redshift)
+        pixel_area = dist_source ** 2 * self.wcs.proj_plane_pixel_area().to(units.rad ** 2) / units.rad ** 2
+        values, counts = np.unique(pixel_map, return_counts=True)
+        values_counts = dict(zip(values, counts))
+        return pixel_area * values_counts[1]
+
